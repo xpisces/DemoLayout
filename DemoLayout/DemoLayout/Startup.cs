92f67752b2fca50e6721d60187f60eaa6d28bd50
xpisces <luongrj@hotmail.com>
2016-08-01 20:41:37 +0700

    commit

---
+++ DemoLayout/DemoLayout/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoLayout.Startup))]
+namespace DemoLayout
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
